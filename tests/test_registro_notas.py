159f1c744912f229c580c31cc6efc35117b66a3b
Luisa Londoño <mlondono408@cue.edu.co>
2026-05-22 08:23:33 -0500

    test: agregar tests RED para aprobación de materias

--- tests/test_registro_notas.py
+++ tests/test_registro_notas.py
@@ -36,3 +36,18 @@ def test_no_permitir_nota_mayor_a_cinco():
             semestre="2026-1",
             nota=5.5
         )
+
+def test_estudiante_aprueba_con_nota_mayor_o_igual_a_tres():
+    service = RegistroNotasService()
+
+    resultado = service.aprobo_materia(3.0)
+
+    assert resultado is True
+
+
+def test_estudiante_reprueba_con_nota_menor_a_tres():
+    service = RegistroNotasService()
+
+    resultado = service.aprobo_materia(2.9)
+
+    assert resultado is False
